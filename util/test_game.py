1583c67a82bc26a643bbaff29b2051db20aed87c
Lu <luis.maltsis@mng.ch>
2021-01-07 15:29:24 +0100

    rebalanced blocking and bugfixed its visuals

--- util/test_game.py
+++ util/test_game.py
@@ -13,10 +13,10 @@ with open('data/teams/Netherlands.json', encoding='utf-8') as f:
 m = basegame.Match([team1, team2], 2)
 m.print = True
 
-with open('data/replays/tests/Berlin_Keys_vs_Netherland_Ghasts', 'w+') as f:
-		f.write('')
+#with open('data/replays/tests/Berlin_Keys_vs_Netherland_Ghasts', 'w+') as f:
+		#f.write('')
 
 for i in range(1000000):
 	events = m.next()
-	with open('data/replays/tests/Berlin_Keys_vs_Netherland_Ghasts', 'a+') as f:
-		f.write(json.dumps(events) + '\n')
+	#with open('data/replays/tests/Berlin_Keys_vs_Netherland_Ghasts', 'a+') as f:
+	#	f.write(json.dumps(events) + '\n')
