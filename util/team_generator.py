1dd57fea422f0d47f1ca5f1eafda25a34b7cb65c
Lu <luis.maltsis@mng.ch>
2021-07-07 15:26:35 +0200

    made team generation work ad infinitum

--- util/team_generator.py
+++ util/team_generator.py
@@ -2,225 +2,227 @@ import json
 import random
 from basegame.basegame import *
 
-cities = [
-	'Amsterdam',
-	'Glasgow',
-	'Paris',
-	'Berlin',
-	'Hamburg',
-	'Melbourne',
-	'York',
-	'Roman',
-	'Elysium',
-	'Kimberley',
-	'Rio',
-	'Barcelona',
-	'Vienna',
-	'Sun City',
-	'Zurich',
-	'British'
-]
 
-objects = [
-	['👻', 'Ghasts'],
-	['🍳', 'Pans'],
-	['⚡', 'Circuits'],
-	['🗝️', 'Keys'],
-	['🚕', 'Taxis'],
-	['💿', 'Discs'],
-	['🎈', 'Ballons'],
-	['🐉', 'Dragons'],
-	['🦊', 'Foxes'],
-	['🎷', 'Saxophones'],
-	['⛓️', 'Chains'],
-	['🪁', 'Kites'],
-	['🥚', 'Eggs'],
-	['🔋',  'Batteries'],
-	['🎀', 'Ribbons'],
-	['🧲', 'Magnets']
-]
+def generate_teams():
+	cities = [
+		'Amsterdam',
+		'Glasgow',
+		'Paris',
+		'Berlin',
+		'Hamburg',
+		'Melbourne',
+		'York',
+		'Roman',
+		'Elysium',
+		'Kimberley',
+		'Rio',
+		'Barcelona',
+		'Vienna',
+		'Sun City',
+		'Zurich',
+		'British'
+	]
 
-player_names = [
-	'Penguin',
-	'Pitch',
-	'Jaxon',
-	'Dry',
-	'Ayla',
-	'Wall',
-	'Syllia',
-	'Umeris',
-	'Sibren',
-	'Veenhuis',
-	'Labyrinth',
-	'Quinn',
-	'Chaos',
-	'Fade',
-	'Echo',
-	'Nox',
-	'Charles',
-	'Magnus',
-	'Kaiser',
-	'Drach',
-	'Tretan' 
-	'Boradil',
-	'Gacco',
-	'Bi',
-	'Violet'
-	'Froid',
-	'Pink',
-	'Mechanical',
-	'Jeanne',
-	'Luna',
-	'Shady',
-	'Angel',
-	'Skeletal',
-	'Curator',
-	'Royal',
-	'Mandrake',
-	'Brown',
-	'Cobra',
-	'Liang',
-	'Huan',
-	'Taute',
-	'Wildmane',
-	'Gwentin',
-	'Tizwyn',
-	'Nymph',
-	'Wynter',
-	'Mario',
-	'Vogl',
-	'Caspian',
-	'Lyn',
-	'Brodir',
-	'Jonsson',
-	'Kinesh',
-	'Freiburg',
-	'Omen',
-	'Mask',
-	'Lindsay',
-	'Chalice',
-	'Noël',
-	'Clair',
-	'Lydia',
-	'Plouffe',
-	'Gus',
-	'Norman',
-	'Hegel',
-	'Beans',
-	'Forest',
-	'Rock',
-	'Tree',
-	'River',
-	'Hill',
-	'House',
-	'Rabbit',
-	'Elbiranth',
-	'Zanai',
-	'Siaphas',
-	'Xypais',
-	'Camruss',
-	'Brave',
-	'Gifted',
-	'True',
-	'Crimson',
-	'Angel',
-	'Blind',
-	'Yellow',
-	'Adam',
-	'Eren',
-	'Nes',
-	'Sunu',
-	'Melon',
-	'Jon',
-	'Theobald',
-	'Guinevere',
-	'Maddock',
-	'Eiriol',
-	'Maddock',
-	'Elin',
-	'Glovruk',
-	'Bobby',
-	'Joe',
-	'James',
-	'Jon',
-	'Garfield',
-	'President',
-	'Big',
-	'Holy',
-	'First',
-	'Wraith',
-	'Sparkling',
-	'Joyce',
-	'Barbara',
-	'Theodore',
-	'Ed',
-	'Lela',
-	'Beth',
-	'Bri',
-	'Gracy',
-	'Dog',
-	'Cat',
-	'Rabbit',
-	'Drake',
-	'Bear',
-	'Beaver',
-	'Slayer',
-	'Avenger',
-	'Reaper',
-	'Solver',
-	'Finder',
-	'Friend',
-	'Gamer',
-	'Black',
-	'White',
-	'Red',
-	'Blue',
-	'Purple',
-	'Glass',
-	'Stone',
-	'Wood',
-	'Roland',
-	'Gnill',
-	'Gnugg',
-	'Tustran',
-	'Caelvu',
-	'Pumpblock',
-	'Finebell',
-	'Castfluke',
-	'Niftyclock',
-	'Singlefizz'
-]
+	objects = [
+		['👻', 'Ghasts'],
+		['🍳', 'Pans'],
+		['⚡', 'Circuits'],
+		['🗝️', 'Keys'],
+		['🚕', 'Taxis'],
+		['💿', 'Discs'],
+		['🎈', 'Ballons'],
+		['🐉', 'Dragons'],
+		['🦊', 'Foxes'],
+		['🎷', 'Saxophones'],
+		['⛓️', 'Chains'],
+		['🪁', 'Kites'],
+		['🥚', 'Eggs'],
+		['🔋',  'Batteries'],
+		['🎀', 'Ribbons'],
+		['🧲', 'Magnets']
+	]
+
+	player_names = [
+		'Penguin',
+		'Pitch',
+		'Jaxon',
+		'Dry',
+		'Ayla',
+		'Wall',
+		'Syllia',
+		'Umeris',
+		'Sibren',
+		'Veenhuis',
+		'Labyrinth',
+		'Quinn',
+		'Chaos',
+		'Fade',
+		'Echo',
+		'Nox',
+		'Charles',
+		'Magnus',
+		'Kaiser',
+		'Drach',
+		'Tretan' 
+		'Boradil',
+		'Gacco',
+		'Bi',
+		'Violet'
+		'Froid',
+		'Pink',
+		'Mechanical',
+		'Jeanne',
+		'Luna',
+		'Shady',
+		'Angel',
+		'Skeletal',
+		'Curator',
+		'Royal',
+		'Mandrake',
+		'Brown',
+		'Cobra',
+		'Liang',
+		'Huan',
+		'Taute',
+		'Wildmane',
+		'Gwentin',
+		'Tizwyn',
+		'Nymph',
+		'Wynter',
+		'Mario',
+		'Vogl',
+		'Caspian',
+		'Lyn',
+		'Brodir',
+		'Jonsson',
+		'Kinesh',
+		'Freiburg',
+		'Omen',
+		'Mask',
+		'Lindsay',
+		'Chalice',
+		'Noël',
+		'Clair',
+		'Lydia',
+		'Plouffe',
+		'Gus',
+		'Norman',
+		'Hegel',
+		'Beans',
+		'Forest',
+		'Rock',
+		'Tree',
+		'River',
+		'Hill',
+		'House',
+		'Rabbit',
+		'Elbiranth',
+		'Zanai',
+		'Siaphas',
+		'Xypais',
+		'Camruss',
+		'Brave',
+		'Gifted',
+		'True',
+		'Crimson',
+		'Angel',
+		'Blind',
+		'Yellow',
+		'Adam',
+		'Eren',
+		'Nes',
+		'Sunu',
+		'Melon',
+		'Jon',
+		'Theobald',
+		'Guinevere',
+		'Maddock',
+		'Eiriol',
+		'Maddock',
+		'Elin',
+		'Glovruk',
+		'Bobby',
+		'Joe',
+		'James',
+		'Jon',
+		'Garfield',
+		'President',
+		'Big',
+		'Holy',
+		'First',
+		'Wraith',
+		'Sparkling',
+		'Joyce',
+		'Barbara',
+		'Theodore',
+		'Ed',
+		'Lela',
+		'Beth',
+		'Bri',
+		'Gracy',
+		'Dog',
+		'Cat',
+		'Rabbit',
+		'Drake',
+		'Bear',
+		'Beaver',
+		'Slayer',
+		'Avenger',
+		'Reaper',
+		'Solver',
+		'Finder',
+		'Friend',
+		'Gamer',
+		'Black',
+		'White',
+		'Red',
+		'Blue',
+		'Purple',
+		'Glass',
+		'Stone',
+		'Wood',
+		'Roland',
+		'Gnill',
+		'Gnugg',
+		'Tustran',
+		'Caelvu',
+		'Pumpblock',
+		'Finebell',
+		'Castfluke',
+		'Niftyclock',
+		'Singlefizz'
+	]
 
 
-for k in range(16):
-	players = []
-	for i in range(6):
-		if len(player_names) < 1:
-			break
-		else:
-			first = random.choice(player_names)
-			last = random.choice(player_names)
+	for k in range(16):
+		players = []
+		for i in range(6):
+			if len(player_names) < 1:
+				break
+			else:
+				first = random.choice(player_names)
+				last = random.choice(player_names)
 
-		stats = Player.random_stats()
-		player = Player(f'{first} {last}', stats)
-		old_rating = player.rating()/5
-		new_rating = sum([random.random() for i in range(3)])/3
-		for key, value in player.stats.items():
-			player.stats[key] = value/old_rating*new_rating
-		players.append(player)
+			stats = Player.random_stats()
+			player = Player(f'{first} {last}', stats)
+			old_rating = player.rating()/5
+			new_rating = sum([random.random() for i in range(3)])/3
+			for key, value in player.stats.items():
+				player.stats[key] = value/old_rating*new_rating
+			players.append(player)
 
-	city = random.choice(cities)
-	cities.remove(city)
-	obj = random.choice(objects)
-	objects.remove(obj)
+		city = random.choice(cities)
+		cities.remove(city)
+		obj = random.choice(objects)
+		objects.remove(obj)
 
-	team = Team(f'{obj[0]} {city} {obj[1]}', players)
-	sum_stats = team.sum_stats()
+		team = Team(f'{obj[0]} {city} {obj[1]}', players)
+		sum_stats = team.sum_stats()
 
-	for player in team.players:
-		for key, value in player.stats.items():
-			player.stats[key] = value/sum_stats*42
+		for player in team.players:
+			for key, value in player.stats.items():
+				player.stats[key] = value/sum_stats*42
 
-	first_word = city.split()[0]
-	with open(f'data/teams/{first_word}.json', 'w+') as f:
-		json.dump(team.export_team(), f, indent=4)
+		first_word = city.split()[0]
+		with open(f'data/teams/{first_word}.json', 'w+') as f:
+			json.dump(team.export_team(), f, indent=4)
--- website/flasksite/game_schedule.py
+++ website/flasksite/game_schedule.py
@@ -11,103 +11,118 @@ from datetime import datetime
 from basegame import basegame
 from util.config_handling import load_config
 from util.webhook_handling import send_message
+from util.team_generator import generate_teams
+from util.populate_tournament import populate_tournament
+from util.data_handling import clear_tournament_replays
+
 
 config = load_config()
 DELAY = config['site']['match_tick']
-
-def generate_teams():
-    	
+        
 
 def now_string():
-	now = datetime.now()
-	return now.strftime('%Y.%m.%d_%H.%M.%S')
+    now = datetime.now()
+    return now.strftime('%Y.%m.%d_%H.%M.%S')
 
 
 def match(team1, team2, save_path):
 
-	with open(save_path, 'w+') as f:
-		f.write('')
+    with open(save_path, 'w+') as f:
+        f.write('')
 
-	first_word = team1.split(' ')[1]
-	with open(f'data/teams/{first_word}.json', encoding='utf-8') as f:
-	    team1 = basegame.Team.load_team(json.load(f))
+    first_word = team1.split(' ')[1]
+    with open(f'data/teams/{first_word}.json', encoding='utf-8') as f:
+        team1 = basegame.Team.load_team(json.load(f))
 
-	first_word = team2.split(' ')[1]
-	with open(f'data/teams/{first_word}.json', encoding='utf-8') as f:
-	    team2 = basegame.Team.load_team(json.load(f))
+    first_word = team2.split(' ')[1]
+    with open(f'data/teams/{first_word}.json', encoding='utf-8') as f:
+        team2 = basegame.Team.load_team(json.load(f))
 
-	m = basegame.Match([team1, team2], 2)
-	m.debug = False
-	m.print = False
+    m = basegame.Match([team1, team2], 2)
+    m.debug = False
+    m.print = False
 
-	while True:
-		if m.game is False:
-			event['event'] = 'over'
-		else:
-			event = m.next()
-			event['event'] = 'match'
+    while True:
+        if m.game is False:
+            event['event'] = 'over'
+        else:
+            event = m.next()
+            event['event'] = 'match'
 
-		with open('data/current.json', 'w') as f:
-			json.dump(event, f)
+        with open('data/current.json', 'w') as f:
+            json.dump(event, f)
 
-		with open(save_path, 'a+') as f:
-			f.write(json.dumps(event) + '\n')
+        with open(save_path, 'a+') as f:
+            f.write(json.dumps(event) + '\n')
 
-		time.sleep(DELAY)
-		if event['event'] == 'over':
-			break
+        time.sleep(DELAY)
+        if event['event'] == 'over':
+            break
 
-	return m
+    return m
 
 
 def random_match():
-	names = []
-	for team in os.listdir('data/teams'):
-		with open(os.path.join('data/teams', team)) as f:
-			name = json.load(f)['name']
-		names.append(name)
+    names = []
+    for team in os.listdir('data/teams'):
+        with open(os.path.join('data/teams', team)) as f:
+            name = json.load(f)['name']
+        names.append(name)
 
 
-	t1 = random.choice(names)
-	t2 = random.choice(names)
-	while t2 == t1:
-		t2 = random.choice(names)
+    t1 = random.choice(names)
+    t2 = random.choice(names)
+    while t2 == t1:
+        t2 = random.choice(names)
 
-	print('random match', t1, t2)
-	match(t1, t2, f'data/replays/tests/{now_string()}_{t1.split()[1]}_{t2.split()[1]}')
+    print('random match', t1, t2)
+    match(t1, t2, f'data/replays/tests/{now_string()}_{t1.split()[1]}_{t2.split()[1]}')
 
 
 def advance_tournament():
-	with open('data/tournament.json') as f:
-		tournament_data = json.load(f)['data']
+    with open('data/tournament.json') as f:
+        tournament_data = json.load(f)['data']
 
-	for i, r in enumerate(tournament_data):
-		for j, m in enumerate(r):
-			for k, team in enumerate(m):
-				if team is None:
-					if i == 0:
-						raise Exception('Not enough teams')
-					else:
-						previous_match = (i - 1, j * 2 + k)
-						pr_depth, pr_breadth = previous_match
-						team1, team2 = tournament_data[pr_depth][pr_breadth]
-						
-						print('match', team1, team2)
-						m = match(team1, team2, f'data/replays/tournament/{now_string()}_{team1.split()[1]}_{team2.split()[1]}')
-						send_message(f'{team1} vs {team2} match starting!')
-						winner = m.winner.name
+    for i, r in enumerate(tournament_data):
+        for j, m in enumerate(r):
+            for k, team in enumerate(m):
+                if i == len(tournament_data)-1:
+                    send_message(f'Teams reset.')
+                    generate_teams()
+                    populate_tournament()
+                    clear_tournament_replays()
 
-						tournament_data[i][j][k] = winner
-						tournament_data = {'data': tournament_data}
+                if team is None:
+                    if i == 0:
+                        raise Exception('Not enough teams')
+                    
+                    previous_match = (i - 1, j * 2 + k)
+                    pr_depth, pr_breadth = previous_match
+                    team1, team2 = tournament_data[pr_depth][pr_breadth]
+                    
+                    print('match', team1, team2)
+                    send_message(f'{team1} vs {team2} match starting!')
+                    m = match(team1, team2, f'data/replays/tournament/{now_string()}_{team1.split()[1]}_{team2.split()[1]}')
+                    
+                    if i == len(tournament_data)-2:
+                        send_message(f'{m.winner.name} won the tournament.')
 
-						with open('data/tournament.json', 'w') as f:
-							json.dump(tournament_data, f, indent=4)
-						return
-			else:
-				pass
+                    winner = m.winner.name
 
+                    tournament_data[i][j][k] = winner
+                    tournament_data = {'data': tournament_data}
 
-schedule.every().day.at('13:29').do(random_match)
+                    with open('data/tournament.json', 'w') as f:
+                        json.dump(tournament_data, f, indent=4)
+                    return
+                else:
+                    pass
+
+send_message(f'Teams reset.')
+generate_teams()
+populate_tournament()
+clear_tournament_replays()
+schedule.every().day.at('16:00').do(random_match)
 
 while True:
     schedule.run_pending()
