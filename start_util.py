0527a8a9eedc1c858270f487ca16015d7de4523a
Lu <luis.maltsis@mng.ch>
2021-07-07 15:26:09 +0200

    changed start_util to be a reset

--- start_util.py
+++ start_util.py
@@ -1,2 +1,8 @@
 from util import team_generator
+from util import populate_tournament
+from util import data_handling
+
+team_generator.generate_teams()
+populate_tournament.populate_tournament()
+data_handling.clear_tournament_replays()
 
